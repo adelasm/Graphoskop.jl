a1828738781aed153d0c9923a2f3e96fa91b40ac
losm <losm@itu.dk>
2023-02-09 14:43:12 +0100

    implemented solverInterface.

--- src/Graphoskop.jl
+++ src/Graphoskop.jl
@@ -8,4 +8,7 @@ include("laplacian_solvers/approxCholTypes.jl")
 include("laplacian_solvers/approxChol.jl")
 export approxchol_lap, ApproxCholParams, approxchol_sddm
 
+include("laplacian_solvers/solverInterface.jl")
+export chol_sddm, chol_lap, lapWrapSDDM
+
 end
