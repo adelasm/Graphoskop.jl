6f86866d96611a5754a4383b5e1b79b2ee313b53
losm <losm@itu.dk>
2023-05-03 19:04:38 +0200

    Added different testing set

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using DataFrames
 using Laplacians
 using SparseArrays
 
-@testset "Graphoskop.jl" begin
+@testset "ge on big data sets" begin
    attributes = pwd() * "\\data\\reddit_11_2016_line_node_attributes.csv"
    graph = pwd() * "\\data\\reddit_11_2016_linegraph.csv"
 
@@ -26,3 +26,23 @@ using SparseArrays
    @test round(of_result,digits=3) == 3.456
 
 end
+
+@testset "ge on smaller data sets" begin
+    attributes = pwd() * "\\data\\small_test_node_attributes.csv"
+    graph = pwd() * "\\data\\small_test_edges.csv" 
+ 
+    df = DataFrame(CSV.File(open(graph))); 
+    attributes = DataFrame(CSV.File(open(attributes)));
+    size = max(maximum(df.src), maximum(df.trg))
+    G = SimpleGraph(size);
+    for row in eachrow(df)
+     add_edge!(G, row.src, row.trg);
+    end
+ 
+    id_result = Graphoskop.ge(G, vec(Array(select(attributes, :ideology_difference => AsTable))));
+    of_result = Graphoskop.ge(G, vec(Array(select(attributes, :offensiveness => AsTable))));
+    @test round(id_result,digits=3) == 0.726
+    @test round(of_result,digits=3) == 1.127
+ 
+
+ end
